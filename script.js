e409c9e23051f6cc752612c78356170d25892ba2
Anish Kumar <anishn9999@gmail.com>
2026-09-20 22:20:47 +0530

    Add typing animation

--- script.js
+++ script.js
@@ -150,3 +150,76 @@ const year =
     new Date().getFullYear();
 
 console.log("Current Year:", year);
+// ================================
+// TYPING ANIMATION
+// ================================
+
+const typingText =
+    document.getElementById("typing-text");
+
+const roles = [
+    "B.Tech CSE Student",
+    "Java & DSA Enthusiast",
+    "Web Developer",
+    "Problem Solver"
+];
+
+let roleIndex = 0;
+let characterIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+
+    const currentRole =
+        roles[roleIndex];
+
+    if (!deleting) {
+
+        typingText.textContent =
+            currentRole.substring(
+                0,
+                characterIndex + 1
+            );
+
+        characterIndex++;
+
+        if (characterIndex === currentRole.length) {
+
+            deleting = true;
+
+            setTimeout(typeEffect, 1500);
+
+            return;
+        }
+
+    } else {
+
+        typingText.textContent =
+            currentRole.substring(
+                0,
+                characterIndex - 1
+            );
+
+        characterIndex--;
+
+        if (characterIndex === 0) {
+
+            deleting = false;
+
+            roleIndex++;
+
+            if (roleIndex === roles.length) {
+                roleIndex = 0;
+            }
+
+        }
+
+    }
+
+    setTimeout(
+        typeEffect,
+        deleting ? 60 : 100
+    );
+}
+
+typeEffect();
